822d7cb228a324031963a614cb1409fc628d1b5c
Shubham Garg <shubham@agrowave.in>
2022-11-09 18:01:34 +0530

    added get file api

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,19 +1,30 @@
-import { Controller, Delete, Get, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { Controller, Delete, Get, Post, Query, Req, Res, StreamableFile, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { Request, Response } from 'express';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) { }
 
-  @Get()
+  @Get('/buckets')
   listBuckets() {
     return this.appService.listBuckets();
+  }
+  @Get('/file')
+  async getFile(@Req() req: Request, @Res() res: Response, @Query('download') download: string, @Query('fileName') fileName: string) {
+    const data = await this.appService.getFile(fileName);
+    if( download && download.toLowerCase() === 'true') {
+      res.set({
+        'Content-Disposition': `attachment; filename=${fileName}`,
+      });
+    }
+    return data.pipe(res);
   }
 
   @Get('/files')
   listFiles() {
-    return this.appService.getFiles();
+    return this.appService.listFiles();
   }
 
   @Post('upload')
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,5 +1,6 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { S3 } from 'aws-sdk';
+import { Readable } from 'stream';
 
 @Injectable()
 export class AppService {
@@ -37,7 +38,21 @@ export class AppService {
     }
   }
 
-  async getFiles() {
+  async getFile(fileName: string) {
+    try {
+      let s3 = new S3();
+      const body = await s3.getObject({Bucket: 'node-example-bucket', Key: fileName}).promise(); 
+      const buffer = Buffer.from(body.Body.valueOf() as Buffer);
+      return Readable.from(buffer);
+    } catch (error) {
+      console.log('inside catch')
+      console.log(error);
+      throw new BadRequestException(error.message)
+    }
+  }
+
+
+  async listFiles() {
     try {
       let s3 = new S3();
       return await (await s3.listObjects({Bucket: 'node-example-bucket'}).promise()).Contents;   
